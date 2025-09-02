38c4cfb1cd62048d93784e3abbd0dce95f17a14e
Elliott Hillary <git@elliott.thehillarys.uk>
2025-09-02 21:43:55 +0100

    Fix ordering

--- Sources/handler.swift
+++ Sources/handler.swift
@@ -11,8 +11,12 @@ class Handler {
         proxy: CGEventTapProxy, type: CGEventType, event: CGEvent, refcon: UnsafeMutableRawPointer?
     ) -> Unmanaged<CGEvent>? {
         let eventSourcePid = event.getIntegerValueField(.eventSourceUnixProcessID)
-        let keyCode = event.getIntegerValueField(.keyboardEventKeycode)
+        if eventSourcePid == pid {
+            // Leave any events that we have emitted alone
+            return Unmanaged.passUnretained(event)
+        }
 
+        let keyCode = event.getIntegerValueField(.keyboardEventKeycode)
         switch type {
         case .keyUp where keyCode == kVK_Escape:
             escDown = false
@@ -22,21 +26,21 @@ class Handler {
                 down.setIntegerValueField(.eventSourceUnixProcessID, value: pid)
                 down.post(tap: .cghidEventTap)
                 escEvent = nil
-                return Unmanaged.passUnretained(event)
+
+                // We have to post the keyUp event, rather than returning it,
+                // otherwise it can get reordered and appear before the keyDown!
+                event.setIntegerValueField(.eventSourceUnixProcessID, value: pid)
+                event.post(tap: .cghidEventTap)
+                return nil
             } else {
                 // Emit nothing. We were acting as modifier keys
                 return nil
             }
         case .keyDown where keyCode == kVK_Escape:
-            if eventSourcePid == pid {
-                // Leave our escape keyDown's alone
-                return Unmanaged.passUnretained(event)
-            } else {
-                // Real escape keyDown
-                escDown = true
-                escEvent = event
-                return nil
-            }
+            // Real escape keyDown, store it for later
+            escDown = true
+            escEvent = event
+            return nil
         case .keyUp, .keyDown:
             // If we're used as hyper, then we're not escape, so we won't emit
             // escape keypresses for this press
