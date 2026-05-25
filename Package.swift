805b8d34e3061e3af9ee9b8f4dbe6299fc5d8d74
Elliott Hillary <git@elliott.thehillarys.uk>
2026-05-25 11:04:47 +0100

    Add Info.plist

--- Package.swift
+++ Package.swift
@@ -7,7 +7,15 @@ let package: Package = Package(
     name: "hypr",
     targets: [
         .executableTarget(
-            name: "hypr"
+            name: "hypr",
+            linkerSettings: [
+                .unsafeFlags([
+                    "-Xlinker", "-sectcreate",
+                    "-Xlinker", "__TEXT",
+                    "-Xlinker", "__info_plist",
+                    "-Xlinker", "Sources/Info.plist",
+                ])
+            ]
         )
     ]
 )
